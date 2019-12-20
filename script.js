ecbc1304d484995edff963fcfd32bea2e29e0b0a
Noah Connors <noahconnors@mnmsa.org>
2019-12-19 20:59:22 -0600

    Add track sorting and height computing

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function getSpacetimeEvents(logs) {
         if(log.message === "Spacetime Start" || log.message === "Spacetime End") {
             const eventName = log.fields.find(field => field.name === "EventName").value
             const id = log.fields.find(field => field.name === "ID").value
-            console.log(eventName)
             if(log.message === "Spacetime Start") {
                 inProgressEvents[id] = {
                     name: eventName,
@@ -52,8 +51,61 @@ function getSpacetimeEvents(logs) {
     return spacetimeEvents
 }
 
+function sortIntoTracks(spacetimeEvents) {
+    const sortedTracks = []
+    for(const event of spacetimeEvents) {
+        let trackIndex = 0
+        while(trackIndex < sortedTracks.length) {
+            const track = sortedTracks[trackIndex]
+            let overlapped = false
+            for(const trackEvent of track) {
+                if(doEventsOverlap(event, trackEvent)) {
+                    overlapped = true
+                    break
+                }
+            }
+            if(!overlapped) {
+                track.push(event)
+                break
+            } else {
+                trackIndex++
+            }
+        }
+
+        if(trackIndex >= sortedTracks.length) {
+            sortedTracks.push([event])
+        } 
+    }
+
+    return sortedTracks
+}
+
+function getHeightOfEvents(events) {
+    sortedTracks = sortIntoTracks(events)
+    let height = 0
+    for(const track of sortedTracks) {
+        const eventHeights = track.map(event => getHeightOfEvent(event))
+        const maxHeight = eventHeights.reduce((currentMax, height) => height > currentMax ? height : currentMax)
+        console.log(eventHeights, maxHeight, track.map(event => event.name))
+        height += maxHeight
+    }
+
+    return height
+}
+
+function getHeightOfEvent(event) {
+    return 1 + getHeightOfEvents(event.children)
+}
+
+function doEventsOverlap(event1, event2) {
+    return event1.startTime < event2.endTime && event1.endTime > event2.startTime
+}
+
 (async () => {
-    const logs = await loadMatch(1)
+    const logs = await loadMatch(3)
+    const events = getSpacetimeEvents(logs)
     console.log(logs)
-    console.log(getSpacetimeEvents(logs))
+    console.log(events)
+    console.log(sortIntoTracks(events))
+    console.log(getHeightOfEvents(events))
 })()
